614c03355999dd1be5f7e69b8f37a22886bf8ca3
Pontes <hpo.pontes@gmail.com>
2023-06-19 12:12:53 -0300

    validity State

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const camposDoFormulario = document.querySelectorAll('[required]')
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", evento => evento.preventDefault())
 })
 
 function verificaCampo(campo) {
