15c496dd0a68683d9c16d8613f5a9adca12183ce
Pontes <hpo.pontes@gmail.com>
2023-06-19 11:44:05 -0300

    validacao de idade

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 import ehUmCPF from "./valida-cpf.js";
-const camposDoFormulario = document.querySelectorAll('[required]');
-camposDoFormulario.forEach((campo)=>{
-    campo.addEventListener('blur', ()=> verificaCampo(campo));
+import ehMaiorDeIdade from "./valida-idade.js";
+const camposDoFormulario = document.querySelectorAll('[required]')
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo));
 })
-function verificaCampo(campo){
-    if(campo.name == 'cpf' && campo.value.length >= 11){
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPF(campo);
+    }
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo);
     }
 }
--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,50 +1,61 @@
-export default function ehUmCPF(campo){
-    const cpf = campo.value.replace(/\.|-/g, '')
-    //console.log(cpf)
+export default function ehUmCPF(campo) {
+    const cpf = campo.value.replace(/\.|-/g, "");
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse cpf não existe!")
+        console.log("Esse cpf não existe")
     } else {
-            console.log("Existe!")
+        console.log("Esse cpf existe")
     }
 }
+
 function validaNumerosRepetidos(cpf) {
     const numerosRepetidos = [
-    '00000000000',
-    '11111111111',
-    '22222222222',
-    '33333333333',
-    '44444444444',
-    '55555555555',
-    '66666666666',
-    '77777777777',
-    '88888888888',
-    '99999999999'
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
     ]
-    return numerosRepetidos.includes(cpf);
+
+    return numerosRepetidos.includes(cpf)
 }
-function validaPrimeiroNumeroCPF(cpf){
+
+function validaPrimeiroDigito(cpf) {
     let soma = 0;
-    let mult = 10;
-    for(let tamanho = 0; tamanho <9 ; tamanho ++){
-        soma += cpf[tamanho] * mult
-        mult--
+    let multiplicador = 10;
+
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
     }
-    soma = (soma *10) % 11;
-    if(soma==10 || soma == 11){
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
         soma = 0;
     }
+
     return soma != cpf[9];
 }
-function validaSegundoNumeroCPF(cpf){
+
+function validaSegundoDigito(cpf) {
     let soma = 0;
-    let mult = 11;
-    for(let tamanho = 0; tamanho <10 ; tamanho ++){
-        soma += cpf[tamanho] * mult
-        mult--
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
     }
-    soma = (soma *10) % 11;
-    if(soma==10 || soma == 11){
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
         soma = 0;
     }
+
     return soma != cpf[10];
 }
