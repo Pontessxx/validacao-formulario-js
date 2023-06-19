28c1e853af146c4522703026679fe364bf6515d2
Pontes <hpo.pontes@gmail.com>
2023-06-19 11:33:31 -0300

    validacao de cpf funcionando

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,4 +1,50 @@
 export default function ehUmCPF(campo){
     const cpf = campo.value.replace(/\.|-/g, '')
-    console.log(cpf)
+    //console.log(cpf)
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log("Esse cpf não existe!")
+    } else {
+            console.log("Existe!")
+    }
+}
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999'
+    ]
+    return numerosRepetidos.includes(cpf);
+}
+function validaPrimeiroNumeroCPF(cpf){
+    let soma = 0;
+    let mult = 10;
+    for(let tamanho = 0; tamanho <9 ; tamanho ++){
+        soma += cpf[tamanho] * mult
+        mult--
+    }
+    soma = (soma *10) % 11;
+    if(soma==10 || soma == 11){
+        soma = 0;
+    }
+    return soma != cpf[9];
+}
+function validaSegundoNumeroCPF(cpf){
+    let soma = 0;
+    let mult = 11;
+    for(let tamanho = 0; tamanho <10 ; tamanho ++){
+        soma += cpf[tamanho] * mult
+        mult--
+    }
+    soma = (soma *10) % 11;
+    if(soma==10 || soma == 11){
+        soma = 0;
+    }
+    return soma != cpf[10];
 }
